84eaac26d02188ed8f42901f8ec857f9c363ab51
peng.qiao <15829265520@163.com>
2018-07-28 22:48:53 +0900

    追加系统开放时间设置

--- docker/mysql/init/mysql-init.sql
+++ docker/mysql/init/mysql-init.sql
@@ -1,5 +1,13 @@
 use mvnbook;
 
+drop table if exists online_time;
+create table online_time (
+  id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  StartTime bigint unsigned not null default 0,
+  EndTime bigint unsigned not null default 0,
+) DEFAULT CHARSET=utf8;
+insert into online_time (StartTime, EndTime) values (1467725966324, 1467725966324);
+
 drop table if exists navigate;
 create table navigate (
   id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -19,11 +27,11 @@ create table navigate (
   PRIMARY KEY (id)
 ) DEFAULT CHARSET=utf8;
 
-LOCK TABLES navigate WRITE;
 insert into navigate (treeid,text,state,checked,iconCls,power,attr_level,attr_isleaf,attr_parents,attr_url,CreateTime,UpdateTime,Status) values
 (9,'系统管理','closed',0,'icon-system',1,1,0,0,'#',1467725966324,1467725966324,'N'),
 (901,'系统角色管理','open',0,'icon-role',1,2,1,9,'sysrole/sysroles.html',1467725966324,1467725966324,'N'),
 (902,'系统帐号管理','open',0,'icon-user',1,2,1,9,'sysrole/sysaccts.html',1467725966324,1467725966324,'N'),
+(903,'系统开放日期','open',0,'icon-lock',1,2,1,9,'sysrole/sysonline.html',1467725966324,1467725966324,'N'),
 (990,'系统导航栏管理','open',0,'icon-tree',0,2,1,9,'sysrole/navmanager.html',1467725966324,1467725966324,'N');
 insert into navigate (treeid,text,state,checked,iconCls,power,attr_level,attr_isleaf,attr_parents,attr_url,CreateTime,UpdateTime,Status) values
 (8,'基础表数据','closed',0,'icon-db',1,1,0,0,'#',1467725966324,1468065780651,'U'),
@@ -46,14 +54,13 @@ insert into navigate (treeid,text,state,checked,iconCls,power,attr_level,attr_is
 (701,'评价信息录入','open',0,'icon-statistics',1,2,1,7,'appraise/appraise.html',1467725966324,1467725966324,'D'),
 (702,'推荐查询','open',0,'icon-statistics',2,2,1,7,'appraise/apprec.html',1467881114342,1467883292793,'D'),
 (703,'评价详情','open',0,'icon-statistics',2,2,1,7,'appraise/appdetail.html',1468035767706,1468035767706,'N'),
-(704,'使用历史','open',0,'icon-history',2,2,1,7,'appraise/history.html',1468035879079,1468036148591,'U'),
+(704,'使用历史','open',0,'icon-history',2,2,1,7,'appraise/history.html',1468035879079,1468036148591,'N'),
 (705,'教材查询','open',0,'icon-book',2,2,1,7,'table/books.html',1468065859879,1468065859879,'D'),
 (706,'调查表','open',0,'',2,2,1,7,'appraise/newappraise.html',1479229798988,1479229798988,'D'),
 (707,'班级统计','open',0,'icon-user',2,2,1,7,'appraise/glasstj.html',1479229798988,1479229798988,'N'),
 (708,'教材统计','open',0,'icon-book',2,2,1,7,'appraise/jiaocaitj.html',1479229798988,1479229798988,'N'),
 (709,'教师统计','open',0,'icon-role',2,2,1,7,'appraise/jiaoshitj.html',1479229798988,1479229798988,'N'),
 (890,'Excel导入','open',0,'icon-table',1,2,1,8,'table/importfile.html',1480085209153,1480085248032,'U');
-UNLOCK TABLES;
 
 drop table if exists account;
 create table account (
@@ -81,7 +88,7 @@ create table roles (
   PRIMARY KEY (id)
 ) DEFAULT CHARSET=utf8;
 insert into roles (code,value,CreateTime,UpdateTime,Status) values (1,'管理员',1467725627980,1467725627980,'N');
-insert into roles (code,value,CreateTime,UpdateTime,Status) values (2,'教材',1467725627980,1467725627980,'N');
+insert into roles (code,value,CreateTime,UpdateTime,Status) values (2,'教师',1467725627980,1467725627980,'N');
 
 drop table if exists diaocha_teacher;
 create table diaocha_teacher (
@@ -171,6 +178,8 @@ create table diaocha_result_uuid (
   key idx_diaocha_result_uuid_isbn (isbn),
   key idx_diaocha_result_uuid_jiaoshi (jiaoshi_id)
 ) DEFAULT CHARSET=utf8 comment='以班级教材生成统计结果';
+alter table diaocha_result_uuid modify column Scomments longtext;
+alter table diaocha_result_uuid modify column Tcomments longtext;
 
 drop table if exists diaocha_jiaocai;
 create table diaocha_jiaocai (
---
+++ docker/tomcat/webapps/mvnbook/sysrole/sysonline.js
@@ -0,0 +1,36 @@
+/**
+ * Created by qiaopeng on 18-7-28.
+ */
+$(function(){
+    $(window).resize(function(){
+        $('#sysonline_dg').datagrid('resize');
+    });
+    searchOnlineInfo();
+});
+
+// 获取系统开放时间
+function searchOnlineInfo(){
+    var url = 'async/nav/getonline.cc';
+    var queryParams = {'jiaocai':'','banji':'','jiaoshi':''};
+    loadDataGrid_data('sysonline_dg', url, queryParams, 'GET');
+    $.get(url, function(resp){
+        if(200 == resp.code) {
+            dateRange = resp.data;
+            $('#startDate').datebox('setValue', dateRange[0]);	// set datebox value
+            $('#endDate').datebox('setValue', dateRange[1]);
+        }
+    });
+}
+
+function editonline() {
+    var startDate = $('#startDate').datebox('getValue');
+    var endDate = $('#startDate').datebox('getValue');
+    var url = 'async/nav/uptonline.cc';
+    $.post(url, {startTime: startDate, endTime: endDate}, function(resp){
+        if(200 == resp.code) {
+            $.messager.alert('提示','更新成功','info');
+        } else {
+            $.messager.alert('提示',resp.msg,'error');
+        }
+    });
+}
